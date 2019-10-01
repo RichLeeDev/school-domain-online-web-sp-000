40450507c6909c1aece29c1d772a468c15b2ca86
Richard Lee <richlee81192@gmail.com>
2019-10-01 04:16:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,15 @@
 class School
-  attr_accessor :roster, :length
+  attr_accessor :roster
   
   def initialize(name)
     @name = name
+  end
+  
+  def length=(length)
+    @length = length
+  end
+  
+  def length
+    @length
   end 
 end 
